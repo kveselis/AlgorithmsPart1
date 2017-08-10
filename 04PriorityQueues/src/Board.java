a9ea6913dbe2ad06217369a51b5013872c28c9f2
Arturas Kveselis <kveselis@gmail.com>
2017-08-10 11:11:16 +0200

    git index cleanup

--- 04PriorityQueues/src/Board.java
+++ 04PriorityQueues/src/Board.java
@@ -1,3 +1,4 @@
+import edu.princeton.cs.algs4.StdOut;
 
 public class Board {
     public Board(int[][] blocks)           // construct a board from an n-by-n array of blocks
@@ -8,38 +9,50 @@ public class Board {
     public int dimension()                 // board dimension n
     {
 
+        return 0;
     }
     public int hamming()                   // number of blocks out of place
     {
-
+        return 0;
     }
     public int manhattan()                 // sum of Manhattan distances between blocks and goal
     {
-
+        return 0;
     }
     public boolean isGoal()                // is this board the goal board?
     {
-
+        return true;
     }
     public Board twin()                    // a board that is obtained by exchanging any pair of blocks
     {
-
+        return null;
     }
     public boolean equals(Object y)        // does this board equal y?
     {
-
+        return true;
     }
     public Iterable<Board> neighbors()     // all neighboring boards
     {
-
+        return null;
     }
     public String toString()               // string representation of this board (in the output format specified below)
     {
-
+        return "";
     }
+
+    public static long gcd(long p, long q)
+    {
+        if (q == 0) return p;
+        long r = p % q;
+        return gcd(q, r);
+    }
+
 
     public static void main(String[] args) // unit tests (not graded)
     {
-
+        StdOut.println("Just testing");
+        long p = 1293192312939123;
+        long q = 23939230942309333;
+        StdOut.println(gcd(p, q));
     }
 }
--- 04PriorityQueues/src/Solver.java
+++ 04PriorityQueues/src/Solver.java
@@ -1,18 +1,26 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.MinPQ;
 
 public class Solver {
     public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
     {}
     public boolean isSolvable()            // is the initial board solvable?
-    {}
+    {
+        return true;
+    }
     public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
-    {}
+    {
+        return 0;
+    }
     public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
-    {}
+    {
+        return null;
+    }
+
     public static void main(String[] args) // solve a slider puzzle (given below)
     {
+
+
         // create initial board from file
         In in = new In(args[0]);
         int n = in.readInt();
